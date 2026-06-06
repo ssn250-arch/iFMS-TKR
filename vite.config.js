f99c81261f9a59eb1d93e7c4db8d431749ae6c03
TsSyed <ssn250@gmail.com>
2026-06-06 19:22:14 +0800

    Upload sistem iFMS siap

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ifms-tkr/', // Tambah baris ni (Mesti letak slash depan dan belakang)
+  base: '/iFMS-TKR/', // Tambah baris ni (Mesti letak slash depan dan belakang)
 })
