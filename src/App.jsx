5ca9d199367a96544c8547a25cc7201b8fa26f61
TsSyed <ssn250@gmail.com>
2026-06-06 17:10:33 +0800

    Initial React Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-600">
+      <h1 className="text-5xl font-bold text-white">
+        iFMS-TKR React Version
+      </h1>
+    </div>
+  );
+}
+
+export default App;
